7b03a20a491a32e00d42cb63f7eb2c21c36536c4
Michael Siejak <msiejak02@gmail.com>
2021-12-22 17:06:34 -0500

    migrate to kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 32
+    buildToolsVersion = "32.0.0"
+
+    defaultConfig {
+        applicationId = "com.msiejak.barstore"
+        minSdk = 25
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            splits.abi.isEnable = false
+            splits.density.isEnable = false
+        }
+        create("dogfood") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    dependenciesInfo {
+        includeInBundle = true
+        includeInApk = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.4.0")
+    implementation("com.google.android.material:material:1.6.0-alpha01")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
+    testImplementation("junit:junit:4.13.2")
+    implementation("com.google.mlkit:barcode-scanning:17.0.1")
+    implementation("com.google.zxing:core:3.4.1")
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "Barstore"
-include ':app'
+include(":app")
