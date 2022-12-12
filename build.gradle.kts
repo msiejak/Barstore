c96570be9335f91b93c3de79e81f7cd61a64dff1
Michael Siejak <msiejak02@gmail.com>
2022-12-11 21:46:22 -0500

    update agp

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,11 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("com.android.tools.build:gradle:7.4.0-rc01")
         classpath("com.google.gms:google-services:4.3.14")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
         classpath("com.google.firebase:perf-plugin:1.4.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.5")
     }
 }
