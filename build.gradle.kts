4ec181b7e51a736a7347029df12d8be26c642017
Michael Siejak <msiejak02@gmail.com>
2022-12-11 21:53:42 -0500

    fix kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
         classpath("com.google.gms:google-services:4.3.14")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
         classpath("com.google.firebase:perf-plugin:1.4.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.5")
     }
 }
